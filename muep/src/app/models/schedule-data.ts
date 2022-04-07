2d288ca8b8d716c4775ebf0ffcf59710ada0e274
mssc89 <oweziz89@gmail.com>
2022-04-07 23:47:58 +0200

    Service dla localStorage, model dla danych o planach

---
+++ muep/src/app/models/schedule-data.ts
@@ -0,0 +1,9 @@
+export class ScheduleData {
+  name: String;
+  values: Array<any>;
+
+  constructor(name, values){
+    this.name = name;
+    this.values = values;
+  }
+}
---
+++ muep/src/app/services/storage.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { ScheduleData } from '../models/schedule-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  getScheduleData(){
+    let schedule = JSON.parse(localStorage.getItem("schedules"))[0];
+    return new ScheduleData(schedule.name, schedule.values);
+  }
+}
