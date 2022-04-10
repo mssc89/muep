7a071eb933994092a970a61eea01fe0505011c52
mssc89 <oweziz89@gmail.com>
2022-04-10 17:24:49 +0200

    Margines dolny przy banerze reklamowym, closes #10

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # mUEP
 Nieoficjalna aplikacja mobilna z planem zajęć (i nie tylko) Uniwersytetu Ekonomicznego w Poznaniu.
+
 Gotowa wersja dostępna jest w sklepie [Google Play](https://play.google.com/store/apps/details?id=com.mssc.muep)
+
 Stworzono z użyciem Ionic (Angular) oraz NodeJS.
--- muep/src/app/ad/ad.component.ts
+++ muep/src/app/ad/ad.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { BrowserTestingModule } from '@angular/platform-browser/testing';
 
 import { AdMob, BannerAdOptions, BannerAdSize, BannerAdPosition, BannerAdPluginEvents, AdMobBannerSize } from '@capacitor-community/admob';
 
@@ -8,22 +9,40 @@ import { AdMob, BannerAdOptions, BannerAdSize, BannerAdPosition, BannerAdPluginE
   styleUrls: ['./ad.component.scss'],
 })
 export class AdComponent implements OnInit {
+  appMargin = 0;
 
   constructor() {
   }
 
   async ngOnInit() {
-    const { status } = await AdMob.trackingAuthorizationStatus();
+    AdMob.initialize({
+      requestTrackingAuthorization: true,
+      testingDevices: [],
+      initializeForTesting: true,
+    });
 
     const options: BannerAdOptions = {
       adId: '',
       adSize: BannerAdSize.ADAPTIVE_BANNER,
       position: BannerAdPosition.BOTTOM_CENTER,
       margin: 0,
-      //isTesting: true
+      isTesting: true
       // npa: true
     };
     AdMob.showBanner(options);
+
+    const resizeHandler = AdMob.addListener(BannerAdPluginEvents.SizeChanged, (info: AdMobBannerSize) => {
+      this.appMargin = info.height;
+      if (this.appMargin > 0) {
+        const body = document.querySelector('body');
+        const bodyStyles = window.getComputedStyle(body);
+        const safeAreaBottom = bodyStyles.getPropertyValue("--ion-safe-area-bottom");
+
+        const app: HTMLElement = document.querySelector('ion-router-outlet');
+
+        app.style.marginBottom = `calc(${safeAreaBottom} + ${this.appMargin}px)`;
+      }
+    });
   }
 
 }
